91be522e7e5b6bec76ae58d4f37d6fb48ea757ef
jysun32 <85902113+jysun32@users.noreply.github.com>
2021-07-06 21:22:34 -0700

    Add files via upload

--- settings.py
+++ settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.pollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
